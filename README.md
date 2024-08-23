0eb533f79ecbaa2e509c67a13799f9dcecdc9e80
julianicruz <julian.iguavita@outlook.com>
2024-08-23 18:55:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hello World | FastAPI | Docker
---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {
+        "Message": "Hello world from FastAPI",
+        "Documentation": "In /docs you can find the documentation of the API",
+    }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, name: str = None):
+    return {"item_id": item_id, "name": name}
